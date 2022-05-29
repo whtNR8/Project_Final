1ff1564f2509dfff3ef9cedbf5057372359013bf
whtNR8 <98993679+whtNR8@users.noreply.github.com>
2022-05-28 22:16:04 -0500

    Add files via upload

---
+++ Project_Blazor/Business/B_Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DataAccess;
+
+namespace Business
+{
+    public class B_Student
+    {
+        public static List<Students> students()
+        {
+            using (var db = new ProjectContext())
+            {
+                return db.Students.ToList();
+            }
+        }
+        public static void CreateStudent(Students oStudent)
+        {
+            using (var db = new ProjectContext())
+            {
+                db.Students.Add(oStudent);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateProduct(Students oStudent)
+        {
+            using (var db = new ProjectContext())
+            {
+                db.Students.Update(oStudent);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Project_Blazor/DataAccess/Migrations/20220529024027_MIgration1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class MIgration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 99);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1078);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6,
+                columns: new[] { "ProductCategory", "ProductName", "ProductPrice", "ProductStock" },
+                values: new object[] { "Alimentos", "Gomitas", 100, 10 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "ProductCategory", "ProductName", "ProductPrice", "ProductStock" },
+                values: new object[] { 9, "Juguetería", "Motos", 10, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "StudentAge", "StudentCareer", "StudentName", "StudentSemester" },
+                values: new object[,]
+                {
+                    { 1, 15, "Medicina", "Julian Alvarez", 6 },
+                    { 3, 34, "Mecánica", "Mateo Fernandez", 8 },
+                    { 5, 7, "Fisioterapia", "Lucas Perez", 78 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6,
+                columns: new[] { "ProductCategory", "ProductName", "ProductPrice", "ProductStock" },
+                values: new object[] { "Juguetería", "Motos", 10, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "ProductCategory", "ProductName", "ProductPrice", "ProductStock" },
+                values: new object[] { 8, "Alimentos", "Gomitas", 100, 10 });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "StudentAge", "StudentCareer", "StudentName", "StudentSemester" },
+                values: new object[,]
+                {
+                    { 4, 34, "Mecánica", "Mateo Fernandez", 8 },
+                    { 99, 7, "Fisioterapia", "Lucas Perez", 78 },
+                    { 1078, 15, "Medicina", "Julian Alvarez", 6 }
+                });
+        }
+    }
+}
